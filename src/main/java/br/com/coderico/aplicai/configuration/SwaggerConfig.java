ffe2f9cbb8b33065178b3eb028b57859ecda065c
Érico <oliv.ericorib@gmail.com>
2025-11-24 20:42:24 -0300

    feat: criar configuração para Swagger

--- src/main/java/br/com/coderico/aplicai/configuration/SwaggerConfig.java
+++ src/main/java/br/com/coderico/aplicai/configuration/SwaggerConfig.java
@@ -15,21 +15,23 @@ public class SwaggerConfig {
 
     @Bean
     OpenAPI openAPI() {
-        var securityScheme = new SecurityScheme()
+        var components = new Components()
+                .addSecuritySchemes("Bearer", createSecurityScheme());
+
+        var securityRequirement = new SecurityRequirement()
+                .addList("Bearer", Arrays.asList("read", "write"));
+
+        return new OpenAPI()
+                .components(components)
+                .addSecurityItem(securityRequirement);
+    }
+
+    private SecurityScheme createSecurityScheme() {
+        return new SecurityScheme()
                 .type(SecurityScheme.Type.HTTP)
                 .scheme("bearer")
                 .bearerFormat("JWT")
                 .in(SecurityScheme.In.HEADER)
                 .name(HttpHeaders.AUTHORIZATION);
-
-        var components = new Components()
-                .addSecuritySchemes("bearer-key", securityScheme);
-
-        var securityRequirement = new SecurityRequirement()
-                .addList("bearer-key", Arrays.asList("read", "write"));
-
-        return new OpenAPI()
-                .components(components)
-                .addSecurityItem(securityRequirement);
     }
 }
