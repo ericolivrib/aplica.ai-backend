09b88da416baef0c666d170406168deef5241688
Érico <oliv.ericorib@gmail.com>
2025-11-24 23:23:05 -0300

    feat: recuperar usuário autenticado para busca e criação de candidaturas

--- src/main/java/br/com/coderico/aplicai/controller/JobApplicationController.java
+++ src/main/java/br/com/coderico/aplicai/controller/JobApplicationController.java
@@ -1,14 +1,17 @@
 package br.com.coderico.aplicai.controller;
 
 import br.com.coderico.aplicai.dto.*;
+import br.com.coderico.aplicai.security.UserAuthenticated;
 import br.com.coderico.aplicai.service.JobApplicationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -20,9 +23,8 @@ public class JobApplicationController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<ApiResponse<JobApplicationCreatedResponse>> createJobApplication(@Valid @RequestBody JobApplicationCreateRequest request) {
-        Long userId = 1L;
-        JobApplicationCreatedResponse application = service.createJobApplication(request, userId);
+    public ResponseEntity<ApiResponse<JobApplicationCreatedResponse>> createJobApplication(@Valid @RequestBody JobApplicationCreateRequest request, @AuthenticationPrincipal UserAuthenticated currentUser) {
+        JobApplicationCreatedResponse application = service.createJobApplication(request, currentUser.getId());
 
         var response = new ApiResponse<>("Candidatura criada com sucesso", application);
         URI location = URI.create("/api/v1/applications/" + application.id());
@@ -33,9 +35,8 @@ public class JobApplicationController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<ApiListResponse<JobApplicationResponse>> getUserApplications() {
-        Long userId = 1L;
-        List<JobApplicationResponse> applications = service.getUserApplications(userId);
+    public ResponseEntity<ApiListResponse<JobApplicationResponse>> getUserApplications(@AuthenticationPrincipal UserAuthenticated currentUser) {
+        List<JobApplicationResponse> applications = service.getUserApplications(currentUser.getId());
 
         String message = applications.isEmpty() ? "Usuário ainda não possui candidaturas" : "Candidaturas do usuário";
 
@@ -44,9 +45,8 @@ public class JobApplicationController {
     }
 
     @GetMapping("/{applicationId}")
-    public ResponseEntity<ApiResponse<JobApplicationResponse>> getJobApplication(@PathVariable Long applicationId) {
-        Long userId = 1L;
-        JobApplicationResponse application = service.getJobApplication(userId, applicationId);
+    public ResponseEntity<ApiResponse<JobApplicationResponse>> getJobApplication(@PathVariable Long applicationId, @AuthenticationPrincipal UserAuthenticated currentUser) {
+        JobApplicationResponse application = service.getJobApplication(currentUser.getId(), applicationId);
 
         var response = new ApiResponse<>("Candidatura solicitada", application);
         return ResponseEntity.ok(response);
